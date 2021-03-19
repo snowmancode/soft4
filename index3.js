9cf73284802a32ceaa39201c94a410049ff351b8
ShowCaseCode <showcasecode@outlook.com>
2021-03-19 00:18:02 -0400

    2nd commit for March 19, 2021

---
+++ index3.js
@@ -0,0 +1,13 @@
+const colors = ['red', 'blue', 'orange'];
+
+for (let color of colors)
+    console.log(color)
+
+
+const villian = {
+    name: 'Lex',
+    age: 26
+}
+
+for (let key in villian)
+    console.log(key);
